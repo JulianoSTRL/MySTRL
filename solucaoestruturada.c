4573bfd0168fbf92ae95584a40dec51bbce478d6
JulianoSTRL <julianotrl97@gmail.com>
2025-06-21 11:09:55 +0000

    math ops

---
+++ solucaoestruturada.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Função genérica para entrada de dados
+void dataentry() {
+    // código para a função dataentry
+}
+ 
+// Função genérica para cálculo da média
+float average() {
+    // código para a função average
+}
+
+// Função genérica para exibir dados
+void showaverage() {
+    // código para a função showaverage
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
