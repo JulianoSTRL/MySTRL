0ec9fefb37eb527f44918e25c7edf9ae3b22ec67
JulianoSTRL <julianotrl97@gmail.com>
2025-06-30 11:19:15 +0000

    2ndcard-config1

--- supertrunfo.c
+++ supertrunfo.c
@@ -5,17 +5,14 @@ int main(){
 
     //Variáveis que serão inseridas pelos usuários;
 
-    int carta1, carta2, turismo1, turismo2, populacao1, populacao2;
+    int carta1, carta2, turismo1, turismo2;
+    float populacao1, populacao2,area1, area2, pib1, pib2;
     char estado1 [50];
     char estado2 [50];
     char cidade1 [50];
     char cidade2 [50];
     char codigo1;
     char codigo2;
-    float area1;
-    float area2;
-    float pib1;
-    float pib2;
 
     printf ("Bem Vindo ao Super Trunfo Países \n");
     printf ("\nPrimeiramente faremos as configurações das suas cartas e para isso preciso da sua ajuda\n");
@@ -36,7 +33,7 @@ int main(){
     scanf ("%[^\n]", cidade1);
 
     printf ("Número de habitantes dessa cidade: ");
-    scanf ("%d", &populacao1);
+    scanf ("%f", &populacao1);
 
     printf ("Agora a área em KM dessa cidade: ");
     scanf ("%f", &area1);
@@ -47,16 +44,57 @@ int main(){
     printf ("Por último, quantos pontos turísticos existem nessa cidade: ");
     scanf ("%d", &turismo1);
 
+    
+
+    //Dados da segunda carta
+
+    printf ("\nAgora, letra entre A e H, e número entre 1 e 4, sem repetir o que foi digitado: ");
+    scanf (" %c%d", &codigo2, &carta2);
+
+    getchar ();
+
+    printf ("O Estado que esta carta irá representar: ");
+    scanf ("%[^\n]", estado2);
+
+    getchar ();
+
+    printf ("O nome da Cidade representada: ");
+    scanf ("%[^\n]", cidade2);
+
+    printf ("Número de habitantes dessa cidade: ");
+    scanf ("%f", &populacao2);
+
+    printf ("Agora a área em KM dessa cidade: ");
+    scanf ("%f", &area2);
+
+    printf ("Por favor, me informe o PIB dessa cidade: ");
+    scanf ("%f", &pib2);
+
+    printf ("Por último, quantos pontos turísticos existem nessa cidade: ");
+    scanf ("%d", &turismo2);
+
     //"Resultado dos dados da carta";
 
-    printf ("\n--- Perfeito, sua primeira carta ficou assim ---\n");
+    printf ("\n--- Sua primeira carta ficou assim ---\n");
     printf ("Carta 1:\n");
     printf ("Estado: %s.\n", estado1);
     printf ("Código: %c%d.\n",codigo1, carta1);
     printf ("Cidade: %s.\n", cidade1);
-    printf ("População: %d mil habitantes.\n", populacao1);
+    printf ("População: %.3f mil habitantes.\n", populacao1);
     printf ("Área: %.3f km².\n", area1);
-    printf ("PIB: %.1fR$ bilhões de reais.\n", pib1);
-    printf ("Pontos turísticos: %d atrações.\n", turismo1); 
+    printf ("PIB: %.3fR$ bilhões de reais.\n", pib1);
+    printf ("Pontos turísticos: %d atrações.\n", turismo1);
+
+    //"Resultado Segunda Carta"
+
+    printf ("\n--- Sua segunda carta ficou assim ---\n");
+    printf ("Carta 2:\n");
+    printf ("Estado: %s.\n", estado2);
+    printf ("Código: %c%d.\n",codigo2, carta2);
+    printf ("Cidade: %s.\n", cidade2);
+    printf ("População: %.3f mil habitantes.\n", populacao2);
+    printf ("Área: %.3f km².\n", area2);
+    printf ("PIB: %.3fR$ bilhões de reais.\n", pib2);
+    printf ("Pontos turísticos: %d atrações.\n", turismo2);
     return 0;
 }
