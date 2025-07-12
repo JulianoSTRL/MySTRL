487aa2f2602c3d1970ddf0d8a4d60b8b1408b515
JulianoSTRL <julianotrl97@gmail.com>
2025-07-12 09:53:42 +0000

    comparison1

---
+++ decisions/comparison.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float val1 = 76.0, val2 = 75.0;
+
+    if (val1 == val2)
+        printf ("Os valores são iguais.");
+    while
+        printf ("Os valores são diferentes");
+return 0;
+}
